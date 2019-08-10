2b40d61f3dbf0a658171b4a2b1ee6d2ca2b8f3c6
Андрей Нечитайло <andreinak@mail.ru>
2019-08-10 15:29:06 +0300

    v 0.1

--- ContactsTabBarTestProject/FirstViewController.m
+++ ContactsTabBarTestProject/FirstViewController.m
@@ -8,16 +8,57 @@
 
 #import "FirstViewController.h"
 
-@interface FirstViewController ()
+
+
+@interface FirstViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property IBOutlet UITableView *firstViewControllerTableView;
+
+@property (strong, nonatomic) NSArray *contactsArray;
 
 @end
+
+
 
 @implementation FirstViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.firstViewControllerTableView.dataSource = self;
+    self.firstViewControllerTableView.delegate = self;
+    
+    self.contactsArray = @[@"Настя", @"Дима", @"Андрей", @"Катя", @"Машка"];
 }
 
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.contactsArray.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString *cellID = @"Cell";
+    static NSString *contactImageName = @"photo";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID];
+    if (!cell)
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellID];
+    
+    cell.textLabel.text = self.contactsArray[indexPath.row];
+    cell.imageView.image = [UIImage imageNamed:contactImageName];
+    
+    return cell;
+}
+
+
+#pragma mark - UITableViewDataDelegate
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 60;
+}
 
 @end
--- ContactsTabBarTestProject/SecondViewController.m
+++ ContactsTabBarTestProject/SecondViewController.m
@@ -8,16 +8,113 @@
 
 #import "SecondViewController.h"
 
-@interface SecondViewController ()
+
+
+@interface SecondViewController () <UITableViewDataSource, UITableViewDelegate, UICollectionViewDataSource, UICollectionViewDelegate>
+
+@property IBOutlet UITableView *secondViewControllerTableView;
+@property (weak, nonatomic) IBOutlet UICollectionView *secondViewControllerCollectionView;
+@property IBOutlet NSLayoutConstraint *collectionViewHeightConstraint;
+
+@property (strong, nonatomic) NSArray *contactsArray;
+@property (strong, nonatomic) NSArray<NSIndexPath *> *selectedRows;
 
 @end
+
+
 
 @implementation SecondViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.secondViewControllerTableView.dataSource = self;
+    self.secondViewControllerTableView.delegate = self;
+    self.secondViewControllerCollectionView.dataSource = self;
+    self.secondViewControllerCollectionView.delegate = self;
+    
+    self.contactsArray = @[@"Настя", @"Дима", @"Андрей", @"Катя", @"Машка"];
+    
+    self.collectionViewHeightConstraint.constant = 0;
+    
+    [self.secondViewControllerTableView setEditing:YES];
 }
+
+- (void)updateTopCollectionViewWithSelectedRows:(NSArray<NSIndexPath *> *)selectedRows {
+    
+    self.collectionViewHeightConstraint.constant = selectedRows.count == 0 ? 0 : 50;
+    
+    [UIView animateWithDuration:0.5f animations:^{
+        [self.view layoutIfNeeded];
+    }];
+    
+    [self.secondViewControllerCollectionView reloadData];
+}
+
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.contactsArray.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString *tableViewCellID = @"tableViewCellID";
+    static NSString *contactImageName = @"photo";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:tableViewCellID];
+    if (!cell)
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:tableViewCellID];
+    
+    cell.textLabel.text = self.contactsArray[indexPath.row];
+    cell.imageView.image = [UIImage imageNamed:contactImageName];
+    
+    return cell;
+}
+
+
+#pragma mark - UITableViewDataDelegate
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 60;
+}
+
+- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 3;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    self.selectedRows = [tableView indexPathsForSelectedRows];
+    [self updateTopCollectionViewWithSelectedRows:self.selectedRows];
+}
+
+- (void)tableView:(UITableView *)tableView didDeselectRowAtIndexPath:(NSIndexPath *)indexPath {
+    self.selectedRows = [tableView indexPathsForSelectedRows];
+    [self updateTopCollectionViewWithSelectedRows:self.selectedRows];
+}
+
+
+#pragma mark - UICollectionViewDataSource
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return self.selectedRows.count;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString *collectionViewCellID = @"collectionViewCellID";
+    
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:collectionViewCellID forIndexPath:indexPath];
+    
+    
+    
+    return cell;
+}
+
+
+#pragma mark - UICollectionViewDataDelegate
 
 
 @end
